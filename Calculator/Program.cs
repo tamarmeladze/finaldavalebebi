46442ccf228bb8fa51745b619676ebe274a90fd4
tamar <tamarmeladze0190@gmail.com>
2024-03-06 17:17:28 +0400

    i added try catch in calculator while  entering text instead of numbers

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,32 +6,37 @@
         {
             while (true)
             {
-                Console.WriteLine("Enter first number");
-                double num1 = double.Parse(Console.ReadLine());
-                Console.WriteLine("Enter second number");
-                double num2 = double.Parse(Console.ReadLine());
-                Console.WriteLine("Enter +,-,* or /");
-                char operation = char.Parse(Console.ReadLine());
+                try
+                {
+                    Console.WriteLine("Enter first number");
+                    double num1 = double.Parse(Console.ReadLine());
 
-                if (operation == '+')
-                    Console.WriteLine("{0}+{1}={2}", num1, num2, num1 + num2);
-                else if (operation == '-')
+                    Console.WriteLine("Enter second number");
+                    double num2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter +,-,* or /");
+                    char operation = char.Parse(Console.ReadLine());
 
-                    Console.WriteLine("{0}-{1}={2}", num1, num2, num1 - num2);
-                else if (operation == '*')
-                    Console.WriteLine("{0}*{1}={2}", num1, num2, num1 * num2);
-                else if (operation == '/')
-                    if (num2 == 0)
-                        Console.WriteLine("can't divide by zero");
+                    if (operation == '+')
+                        Console.WriteLine("{0}+{1}={2}", num1, num2, num1 + num2);
+                    else if (operation == '-')
+
+                        Console.WriteLine("{0}-{1}={2}", num1, num2, num1 - num2);
+                    else if (operation == '*')
+                        Console.WriteLine("{0}*{1}={2}", num1, num2, num1 * num2);
+                    else if (operation == '/')
+                        if (num2 == 0)
+                            Console.WriteLine("can't divide by zero");
+                        else
+                            Console.WriteLine("{0}/{1}={2}", num1, num2, num1 / num2);
                     else
-                        Console.WriteLine("{0}/{1}={2}", num1, num2, num1 / num2);
-                else
-                    Console.WriteLine("Unrecognized character");
+                        Console.WriteLine("Unrecognized character");
 
-                Console.WriteLine("press 'q' to quit or any other key to continue");
-                if (Console.ReadLine() == "q")
-                    break;
-                Console.Clear();
+                    Console.WriteLine("press 'q' to quit or any other key to continue");
+                    if (Console.ReadLine() == "q")
+                        break;
+                    Console.Clear();
+                }
+                catch (FormatException) { Console.WriteLine("enter valid numbers"); }
             }
         }
     }
