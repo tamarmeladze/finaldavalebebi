3f8b1345b91ff4a697576158c0da2204c74272bd
tamar <tamarmeladze0190@gmail.com>
2024-03-06 17:56:01 +0400

    I added try catch to throw exception while user enters invalid simbol

--- Gamocnobana/Program.cs
+++ Gamocnobana/Program.cs
@@ -1,56 +1,67 @@
-﻿namespace Gamocnobana
+﻿using System.Linq.Expressions;
+
+namespace Gamocnobana
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("You have to guess number, first choose the level of game");
-            Console.WriteLine("press 'e' - easy game 1-25 numbers, 'm' - medium game with 1-50numbers;   'h' - hard game 1-100 numbers!");
-            char choice = char.Parse(Console.ReadLine());
-            if (choice != 'e' && choice != 'm' && choice != 'h')
+            while (true)
             {
-                Console.WriteLine("press correct character 'e','m' or 'h'");
-
-            }
-            int minnumber = 1;
-            int maxnumber = 100;
-            if (choice == 'e')
-            {
-                maxnumber = 25;
-            }
-            else if (choice == 'm')
-            {
-                maxnumber = 50;
-            }
-            Console.WriteLine("start guessing the number");
-            Random random = new Random();
-            int numbertoguess = random.Next(minnumber, maxnumber);
-            int tries = 10;
-
-            while (tries > 0)
-            {
-                if (int.TryParse(Console.ReadLine(), out int guess))
+                try
                 {
-                    if (guess == numbertoguess)
+                    Console.WriteLine("You have to guess number, first choose the level of game");
+                    Console.WriteLine("press 'e' - easy game 1-25 numbers, 'm' - medium game with 1-50numbers;   'h' - hard game 1-100 numbers!");
+
+                    char choice = char.Parse(Console.ReadLine());
+                    if (choice != 'e' && choice != 'm' && choice != 'h')
                     {
-                        Console.WriteLine("You Guessed the number !Game Over!");
-                        break;
-                    }
-                    else if (guess < numbertoguess)
-                    {
-                        Console.WriteLine("try higher number");
+                        Console.WriteLine("press correct character 'e','m' or 'h'");
 
                     }
-                    else
+                    int minnumber = 1;
+                    int maxnumber = 100;
+                    if (choice == 'e')
                     {
-                        Console.WriteLine("try lower number");
+                        maxnumber = 25;
                     }
-                    tries--;
+                    else if (choice == 'm')
+                    {
+                        maxnumber = 50;
+                    }
+                    Console.WriteLine("start guessing the number");
+                    Random random = new Random();
+                    int numbertoguess = random.Next(minnumber, maxnumber);
+                    int tries = 10;
+
+                    while (tries > 0)
+                    {
+                         
+                        int guess = int.Parse(Console.ReadLine());
+
+                        if (guess == numbertoguess)
+                        {
+                            Console.WriteLine("You Guessed the number !Game Over!");
+                            break;
+                        }
+                        else if (guess < numbertoguess)
+                        {
+                            Console.WriteLine("try higher number");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("try lower number");
+                        }
+                        tries--;
+                    }
+
+
+                   if (tries == 0)
+                    { Console.WriteLine($"sorry,you lose, the number was {numbertoguess}"); }
                 }
-
+                catch (FormatException) { Console.WriteLine("enter valid digit"); }
             }
-            if (tries == 0)
-            { Console.WriteLine($"sorry,you lose, the number was {numbertoguess}"); }
         }
     }
 }
